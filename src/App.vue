622e25dc9d184ae48e1448d4430e070743be9aa2
EmOtIoN <sunundershadow@gmail.com>
2026-05-20 15:42:04 +0300

    Сделано

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import Article from "@/Article.vue"
+</script>
+
+<template>
+  <Article></Article>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Golos+Text:wght@400..900&display=swap');
+
+</style>
